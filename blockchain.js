60fcdb77a99a3f9aa4ab7d42af42853cbd72617f
danielbarboni <danielbarboni@hotmail.com>
2018-03-18 11:46:07 -0300

    added blockchain tests

--- blockchain.js
+++ blockchain.js
@@ -4,13 +4,13 @@ class Blockchain {
   constructor() {
     this.chain = [Block.genesis()];
   }
-}
 
-addBlock(data) {
-  const block = Block.mineBlock(this.chain[this.chain.lenght-1], data);
-  this.chain.push(block);
+  addBlock(data){
+    const block = Block.mineBlock(this.chain[this.chain.length-1], data);
+    this.chain.push(block);
 
-  return block;
+    return block;
+  }
 }
 
 module.exports = Blockchain;
