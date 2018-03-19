2f7e71543166c3bf477914614e881fbd041df8eb
danielbarboni <danielbarboni@hotmail.com>
2018-03-19 08:48:47 -0300

    added blockchain peers connection capabilities

--- app/index.js
+++ app/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const Blockchain = require('../blockchain');
+const P2pServer = require('./p2p-server');
 
 const HTTP_PORT = process.env.HTTP_PORT || 3001;
 
@@ -9,6 +10,7 @@ const HTTP_PORT = process.env.HTTP_PORT || 3001;
 
 const app = express();
 const bc = new Blockchain();
+const p2pServer = new P2pServer(bc);
 app.use(bodyParser.json());
 
 app.get('/blocks', (req, res) => {
@@ -23,3 +25,4 @@ app.post('/mine', (req, res) => {
 });
 
 app.listen(HTTP_PORT, () => console.log(`Listening on port ${HTTP_PORT}`));
+p2pServer.listen();
